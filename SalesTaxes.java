1bd9f27e2ded0034ca438a16d540aacd085401af
Joshua Chen <joshuahchen@gmail.com>
2014-12-05 13:46:29 -0600

    changed file names

---
+++ SalesTaxes.java
@@ -0,0 +1,79 @@
+import java.util.regex.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public final class SalesTaxes {
+
+	static int salesTax = 10;
+	static int importTax = 5;
+	
+	public static BigDecimal[] calculateItem(String item) {
+		
+		System.out.println("==================");
+		
+		/* first element = tax amount, second element = total amount */
+		BigDecimal[] bd = new BigDecimal[2];
+		BigDecimal totalTaxes;
+		
+		Pattern p = Pattern.compile("[0-9]+[.][0-9]+");
+		Matcher m = p.matcher(item);
+		
+		if (m.find()) {
+			String strAmount = m.group();
+			double amount = Double.parseDouble(strAmount);
+			double sTax = (amount * salesTax / 100.0);
+			
+			BigDecimal b = new BigDecimal(strAmount);
+			BigDecimal b2 = b.setScale(2, BigDecimal.ROUND_UNNECESSARY);
+			System.out.println("BD amount: " + b2);
+			
+			BigDecimal t = new BigDecimal(sTax);
+			BigDecimal t2 = t.setScale(2, BigDecimal.ROUND_CEILING);
+			System.out.println("BD tax: " + t2);
+			
+			totalTaxes = t2;
+			
+			
+			t2 = t2.add(b2);
+			
+			System.out.println("BD subtotal: " + t2);
+			
+			
+			/* See if item is imported or not */
+			Pattern p2 = Pattern.compile("import[A-Za-z]*\\s");
+			Matcher m2 = p2.matcher(item);
+			
+			if (m2.find()) {
+				System.out.println(m2.group());
+				
+				double iTax = (amount * importTax) / 100.0;
+				
+				BigDecimal t3 = new BigDecimal(iTax);
+				BigDecimal t4 = t3.setScale(2, BigDecimal.ROUND_CEILING);
+				System.out.println("import tax: " + t4);
+				
+				totalTaxes = totalTaxes.add(t4);
+				
+				t4 = t4.add(t2);
+				System.out.println("Subtotal 2: " + t4);
+				
+				bd[0] = totalTaxes;
+				bd[1] = t4;
+				
+				System.out.println("total taxes: " + bd[0]);
+				System.out.println("subtotal: " + bd[1]);
+				return bd;
+			}
+			
+			bd[0] = totalTaxes;
+			bd[1] = t2;
+			System.out.println("total taxes: " + bd[0]);
+			System.out.println("subtotal: " + bd[1]);
+			return bd;
+		}
+		
+		else {
+			return bd;
+		}
+	}
+}
---
+++ TestSalesTaxes.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+import org.junit.Assert;
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class TestSalesTaxes {
+
+	@Test
+	public void testBlank() {
+		
+	}
+	
+	@Test
+	public void testOneItemNonExempt() {
+		String oneItem = "1 music CD at 14.99";
+		BigDecimal[] bd = SalesTaxes.calculateItem(oneItem);
+		
+		Assert.assertEquals(bd[0].doubleValue(), 1.50, 0.00);
+		Assert.assertEquals(bd[1].doubleValue(), 16.49, 0.00);
+		
+	}
+	
+	@Test
+	public void testOneItemExempt() {
+		//String oneItem = "1 book at 12.49";
+		//Assert.assertEquals(SalesTaxesJava.calculateItem(oneItem), 12.49, 0.00);
+	}
+	
+	@Test
+	public void testMultItemsExemptNonExempt() {
+		
+	}
+	
+	@Test
+	public void testOneItemImport() {
+		String oneItem = "1 imported bottle of perfume at 27.99";
+		BigDecimal[] bd = SalesTaxes.calculateItem(oneItem);
+		
+		Assert.assertEquals(bd[0].doubleValue(), 4.20, 0.00);
+		Assert.assertEquals(bd[1].doubleValue(), 32.19, 0.00);
+	}
+	
+	@Test
+	public void testMultItemsImport() {
+		
+	}
+	
+	@Test
+	public void testMultItemsExemptNonExemptImport() {
+		
+	}
+	
+	@Test
+	public void testFileOneItem() {
+		
+	}
+	
+	@Test
+	public void testFileMultItems() {
+		
+	}
+}
